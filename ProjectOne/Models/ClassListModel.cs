995fabc12191273a6b399bf040ad65ac26b2ab00
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-02 17:21:48 -0700

    class list layout

--- ProjectOne/Models/ClassListModel.cs
+++ ProjectOne/Models/ClassListModel.cs
@@ -2,5 +2,9 @@
 {
     public class ClassListModel
     {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public decimal ClassPrice { get; set; }
     }
 }
