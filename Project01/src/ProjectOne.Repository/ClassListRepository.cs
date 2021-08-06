93c16570bdc245e00c48be13e0c4a00dda9bd06a
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-06 14:50:36 -0700

    project01 init

---
+++ Project01/src/ProjectOne.Repository/ClassListRepository.cs
@@ -0,0 +1,36 @@
+﻿//using ProjectOne.Models;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace ProjectOne
+//{
+//    public interface IClassListRepository
+//    {
+//        IEnumerable<ClassListModel> ClassList { get; }
+//    }
+
+//    public class ClassListRepository : IClassListRepository
+//    {
+//        public IEnumerable<ClassListModel> ClassList
+//        {
+//            get
+//            {
+//                IEnumerable<ClassListModel> items;
+
+//                var database = new Db.minicstructorContext();
+
+//                items = database.Class
+//                .Select(t => new ClassListModel
+//                {
+//                    ClassId = t.ClassId,
+//                    ClassDescription = t.ClassDescription,
+//                    ClassName = t.ClassName,
+//                    ClassPrice = t.ClassPrice,
+//                })
+//                .ToArray();
+
+//                return items;
+//            }
+//        }
+//    }
+//}
---
+++ Project01/src/ProjectOne.WebSite/Models/ClassListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectOne.Models
+{
+    public class ClassListModel
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public decimal ClassPrice { get; set; }
+    }
+}
