4a4481fa0d57595a2a26759161c12fb95915db26
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-08 12:38:53 -0700

    fixing g.sc errror

---
+++ Project01/src/ProjectOne.WebSite/Models/ClassListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectOne.WebSite.Models
+{
+    public class ClassListModel
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public decimal ClassPrice { get; set; }
+    }
+}
