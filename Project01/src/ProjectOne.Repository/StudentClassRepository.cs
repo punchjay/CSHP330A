cfc9da334c2a5be085383ae3cbb20541211ab66c
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-10 14:22:45 -0700

    wiring up student classes

---
+++ Project01/src/ProjectOne.Repository/StudentClassRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace ProjectOne.Repository
+{
+    public interface IStudentClassRepository
+    {
+        UserClassModel[] UserClasses { get; }
+    }
+
+    public class UserClassModel
+    {
+        public int ClassId { get; set; }
+        public int UserName { get; set; }
+    }
+
+    public class StudentClassRepository : IStudentClassRepository
+    {
+        public UserClassModel[] UserClasses
+        {
+            get
+            {
+                return DatabaseAccessor
+                     .Instance
+                     .UserClass
+                     .Select(t => new UserClassModel
+                     {
+                         ClassId = t.ClassId,
+                         UserName = t.UserId,
+                     })
+                     .ToArray();
+            }
+        }
+    }
+}
