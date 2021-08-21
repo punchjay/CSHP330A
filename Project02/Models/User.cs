e327bbfc6d7f05f12bcdc51b66f4c7d88c8b4534
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-20 18:12:37 -0700

    init project02

---
+++ Project02/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ProjectTwo.Models
+{
+    public class User
+    {
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+
+        [JsonProperty("Email")]
+        public string Email { get; set; }
+
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+
+        [JsonProperty("Date")]
+        public DateTime Date { get; set; }
+    }
+}
