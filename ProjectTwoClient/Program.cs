af7d53c550f88839b68f414aecad952df1f899e0
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-30 18:10:20 -0700

    added console client

---
+++ ProjectTwoClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ProjectTwoClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:9628/api/")
+            };
+
+            var result = client.GetAsync("user").Result;
+            var json = result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+}
