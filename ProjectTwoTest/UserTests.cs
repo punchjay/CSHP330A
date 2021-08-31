c455287f114218bf6604dfd7f4f7f7cc7fbffb9c
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-31 14:20:25 -0700

    added deleted user test

--- ProjectTwoTest/UserTests.cs
+++ ProjectTwoTest/UserTests.cs
@@ -52,5 +52,53 @@ namespace ProjectTwoTest
             //Assert.AreEqual(HttpStatusCode.Created, postResult.StatusCode);
             postResult.StatusCode.Should().Be(HttpStatusCode.Created);
         }
+
+        [Test]
+        public void DeleteUserById_Valid()
+        {
+            var postResult = CreateUser("new-client@uw.org", "clientPassword");
+            var json = postResult.Content.ReadAsStringAsync().Result;
+            var user = JsonConvert.DeserializeObject<User>(json);
+            var result = client.DeleteAsync("user/" + user.Id).Result;
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        //[Test]
+        //public void TestDelete_InvalidContactStringId()
+        //{
+        //    var result = client.DeleteAsync("contacts/abc").Result;
+
+        //    result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        //}
+
+        //[Test]
+        //public void TestDelete_InvalidContactId()
+        //{
+        //    var result = client.DeleteAsync("contacts/1").Result;
+
+        //    result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        //}
+
+        //[Test]
+        //public void TestGetSpecific_Good()
+        //{
+        //    var postResult = CreateContact("TestGetSpecific_Good");
+
+        //    var json = postResult.Content.ReadAsStringAsync().Result;
+
+        //    var contact = JsonConvert.DeserializeObject<Contact>(json);
+
+        //    var result = client.GetAsync("contacts/" + contact.Id).Result;
+
+        //    result.StatusCode.Should().Be(HttpStatusCode.OK);
+        //}
+
+        //[Test]
+        //public void TestGetSpecific_Bad()
+        //{
+        //    var result = client.GetAsync("contacts/10211").Result;
+
+        //    result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        //}
     }
 }
