289dd3773f1812d7d876d9f616a5681e0bff7d1d
Jay Gibble <76593211+punchjay@users.noreply.github.com>
2021-08-30 20:17:16 -0700

    updated post

--- ProjectTwoClient/Program.cs
+++ ProjectTwoClient/Program.cs
@@ -2,6 +2,7 @@
 using ProjectTwoClient.Models;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 
 namespace ProjectTwoClient
@@ -15,9 +16,26 @@ namespace ProjectTwoClient
                 BaseAddress = new Uri("http://localhost:9628/api/")
             };
 
+            var newUser = new User
+            {
+                Email = "new-client@uw.org",
+                Password = "clientPassword",
+            };
+
+            var newJson = JsonConvert.SerializeObject(newUser);
+            var postContent = new StringContent(newJson, System.Text.Encoding.UTF8, "application/json");
+            var postResult = client.PostAsync("user", postContent).Result;
+            if (postResult.StatusCode == HttpStatusCode.Created)
+            {
+                Console.WriteLine(postResult.StatusCode);
+            }
+            else
+            {
+                Console.WriteLine($"{postResult.StatusCode} - PostAsync Failure.");
+            }
+
             var result = client.GetAsync("user").Result;
             var json = result.Content.ReadAsStringAsync().Result;
-
             Console.WriteLine(json);
 
             var list = JsonConvert.DeserializeObject<List<User>>(json);
